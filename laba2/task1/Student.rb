d222b17af20ecf2cb581c56309fb9abf1828ca10
Ryamlin <69631591+Ryamlin@users.noreply.github.com>
2023-05-13 11:19:24 +0300

    Create Student.rb
    Class

---
+++ laba2/task1/Student.rb
@@ -0,0 +1,71 @@
+require 'json'
+class Student
+  public_class_method :new
+  attr_reader :first_name, :second_name, :last_name
+  #открываем сеттеры и геттеры из базового класса
+  public :phone, :telegram, :email, 'id=', 'phone=', 'telegram=', 'email=', 'git=', :set_contacts
+  def initialize(last_name: nil, first_name: nil, second_name: nil, id: nil, phone:nil, telegram: nil, email: nil, git:nil)
+    raise ArgumentError, "Required fields: first_name, second_name and last_name!" if first_name.nil? || second_name.nil?|| last_name.nil?
+    self.last_name=last_name
+    self.first_name=first_name
+    self.second_name=second_name
+    super(id:id, phone:phone, telegram:telegram, email:email, git:git)
+  end
+
+  #конструктор для аргументов в строке
+  def self.from_json_str(str)
+    data=JSON.parse(str).transform_keys(&:to_sym)
+    Student.new(**data)
+  end
+
+  def first_name=(first_name)
+    raise ArgumentError, 'Invalid first_name!' unless first_name.nil? || Student.validate_name?(first_name)
+    @first_name=first_name
+  end
+
+  def second_name=(second_name)
+    raise ArgumentError, 'Invalid second_name!' unless second_name.nil? || Student.validate_name?(second_name)
+    @second_name=second_name
+  end
+
+  def last_name=(last_name)
+    raise ArgumentError, 'Invalid last_name!' unless last_name.nil? || Student.validate_name?(last_name)
+    @last_name=last_name
+  end
+
+  #корректность имени(учтены двойные имена)
+  def self.validate_name?(name)
+    name.match(/^[А-Я][а-я]+(-[А-Я][а-я]+)*$/)
+  end
+
+  #имя с инициалами
+  def short_name
+    "#{last_name} #{first_name[0]}. #{second_name[0]}."
+  end
+  def get_info
+    "#{short_name}, #{find_git}, #{find_contact}"
+  end
+
+  def to_s
+    res = "#{last_name} #{first_name} #{second_name}"
+    res += " id=#{id}" unless id.nil?
+    res += " phone=#{phone}" unless phone.nil?
+    res += " #{find_git}"
+    res += " telegram=#{telegram}" unless telegram.nil?
+    res += " email=#{email}" unless email.nil?
+    res
+  end
+
+  def to_hash
+    info_hash = {}
+    %i[last_name first_name second_name id phone telegram email git].each do |field|
+      info_hash[field] = send(field) unless send(field).nil?
+    end
+    info_hash
+  end
+
+  def to_json_str
+    JSON.generate(to_hash)
+  end
+
+end
