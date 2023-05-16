94155ab5dd43095d2e0ef53d4c5a87eecfa6c535
Ryamlin <69631591+Ryamlin@users.noreply.github.com>
2023-05-16 15:18:15 +0300

    Update Student.rb
    привёл в порядок. класс

--- laba2/task1/Student.rb
+++ laba2/task1/Student.rb
@@ -1,71 +1,85 @@
-require 'json'
 class Student
-  public_class_method :new
-  attr_reader :first_name, :second_name, :last_name
-  #открываем сеттеры и геттеры из базового класса
-  public :phone, :telegram, :email, 'id=', 'phone=', 'telegram=', 'email=', 'git=', :set_contacts
-  def initialize(last_name: nil, first_name: nil, second_name: nil, id: nil, phone:nil, telegram: nil, email: nil, git:nil)
-    raise ArgumentError, "Required fields: first_name, second_name and last_name!" if first_name.nil? || second_name.nil?|| last_name.nil?
-    self.last_name=last_name
-    self.first_name=first_name
-    self.second_name=second_name
-    super(id:id, phone:phone, telegram:telegram, email:email, git:git)
-  end
+attr_accessor :id
+attr_reader :phone, :first_name, :second_name, :last_name, :telegram, :git, :email
+def initialize(last_name:, first_name:, second_name:, id:nil, phone:nil, telegram: nil, git:nil, email: nil )
+  self.last_name=last_name
+  self.first_name=first_name
+  self.second_name=second_name
+  self.id=id
+  self.phone=phone
+  self.telegram=telegram
+  self.git=git
+  self.email=email
+end
+#set phone
+def phone=(phone)
+  raise ArgumentError, 'Invalid phone number!' unless phone.nil? || Student.validate_phone?(phone)
+  @phone = phone
+end
+def first_name=(first_name)
+  raise ArgumentError, 'Invalid first_name!' unless first_name.nil? || Student.validate_name?(first_name)
+  @first_name=first_name
+end
+def second_name=(second_name)
+  raise ArgumentError, 'Invalid second_name!' unless second_name.nil? || Student.validate_name?(second_name)
+  @second_name=second_name
+end
+def last_name=(last_name)
+  raise ArgumentError, 'Invalid last_name!' unless last_name.nil? || Student.validate_name?(last_name)
+  @last_name=last_name
+end
+def telegram=(telegram)
+  raise ArgumentError, 'Invalid telegram!' unless telegram.nil? || Student.validate_account?(telegram)
+  @telegram=telegram
+end
+def git=(git)
+  raise ArgumentError, 'Invalid git!' unless git.nil? || Student.validate_account?(git)
+  @git=git
+end
+def email=(email)
+  raise ArgumentError, 'Invalid email!' unless email.nil? || Student.validate_email?(email)
+  @email=email
+end
+#проверка на номер
+def self.validate_phone?(phone)
+  phone.match(/^(8|\+7)[\- ]?\(?\d{3}\)?[\- ]?\d{3}([\- ]?\d{2}){2}$/)
+end
+#корректность имени(учтены двойные имена)
+def self.validate_name?(name)
+  name.match(/^[А-Я][а-я]+(-[А-Я][а-я]+)*$/)
+end
+#валидация для гита и телеграмма
+def self.validate_account?(account)
+  account.match(/^@[\w-]+$/)
+end
+#валидация для почты
+def self.validate_email?(email)
+  email.match(/^\w+[-\w.]+@([A-Za-z]+\.)+[A-z]{2,4}$/)
+end
+def git?
+  !git.nil?
+end
+def exist_contact?
+  !(telegram.nil? && email.nil? && phone.nil?)
+end
 
-  #конструктор для аргументов в строке
-  def self.from_json_str(str)
-    data=JSON.parse(str).transform_keys(&:to_sym)
-    Student.new(**data)
-  end
+def validate?
+  git? && exist_contact?
+end
+def set_contacts(contacts)
+  self.email = contacts[:email] if contacts.key?(:email)
+  self.phone = contacts[:phone] if contacts.key?(:phone)
+  self.telegram = contacts[:telegram] if contacts.key?(:telegram)
+end
 
-  def first_name=(first_name)
-    raise ArgumentError, 'Invalid first_name!' unless first_name.nil? || Student.validate_name?(first_name)
-    @first_name=first_name
-  end
-
-  def second_name=(second_name)
-    raise ArgumentError, 'Invalid second_name!' unless second_name.nil? || Student.validate_name?(second_name)
-    @second_name=second_name
-  end
-
-  def last_name=(last_name)
-    raise ArgumentError, 'Invalid last_name!' unless last_name.nil? || Student.validate_name?(last_name)
-    @last_name=last_name
-  end
-
-  #корректность имени(учтены двойные имена)
-  def self.validate_name?(name)
-    name.match(/^[А-Я][а-я]+(-[А-Я][а-я]+)*$/)
-  end
-
-  #имя с инициалами
-  def short_name
-    "#{last_name} #{first_name[0]}. #{second_name[0]}."
-  end
-  def get_info
-    "#{short_name}, #{find_git}, #{find_contact}"
-  end
-
-  def to_s
-    res = "#{last_name} #{first_name} #{second_name}"
-    res += " id=#{id}" unless id.nil?
-    res += " phone=#{phone}" unless phone.nil?
-    res += " #{find_git}"
-    res += " telegram=#{telegram}" unless telegram.nil?
-    res += " email=#{email}" unless email.nil?
-    res
-  end
-
-  def to_hash
-    info_hash = {}
-    %i[last_name first_name second_name id phone telegram email git].each do |field|
-      info_hash[field] = send(field) unless send(field).nil?
-    end
-    info_hash
-  end
-
-  def to_json_str
-    JSON.generate(to_hash)
-  end
+def to_s
+  res = "#{last_name} #{first_name} #{second_name}"
+  res += " id=#{id}" unless id.nil?
+  res += " phone=#{phone}" unless phone.nil?
+  res += " git=#{git}" unless git.nil?
+  res += " telegram=#{telegram}" unless telegram.nil?
+  res += " email=#{email}" unless email.nil?
+  res
+end
 
 end
