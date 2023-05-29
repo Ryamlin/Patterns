e85d956a1f5936c040b712b8e6fb724b7db56ad0
Ryamlin <69631591+Ryamlin@users.noreply.github.com>
2023-05-29 20:38:15 +0300

    Create Studentjson.rb
    класс для джисон

---
+++ laba2/task4/Studentjson.rb
@@ -0,0 +1,19 @@
+require_relative 'studentListStrategy'
+require 'json'
+
+class StudentListJson<StudentListStrategy
+    public_class_method :new
+
+
+
+    #получение массива хэшей из строки
+  def list_hash_from_str(str)
+    JSON.parse(str,  {symbolize_names: true })
+  end
+
+  #получение строки заданного вида из массива хэшей
+  def list_hash_to_str(list_hash)
+    JSON.generate(list_hash)
+  end
+
+end
